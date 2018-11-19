ddb0a19791f3d4f3bd25a3b9c6b189333c60dc28
Albert <asdfg51014@gmail.com>
2018-11-20 00:29:06 +0800

    Initial Commit

---
+++ SearchController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchController
+//
+//  Created by Albert on 2018/11/20.
+//  Copyright © 2018 Albert.C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
